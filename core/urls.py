7fb0ac8d33de9592e6c8855a197036a01ef6e78d
Yao Josué <46876900+MJoshua25@users.noreply.github.com>
2024-11-28 16:44:44 +0000

    first view

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
-from django.views.generic import TemplateView
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('', views.IndexView.as_view(), name='index'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.shortcuts import get_object_or_404, get_list_or_404
+from . import models
 
 # Create your views here.
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs) 
+        
+        context['personal_info'] = get_object_or_404(models.PersonalInfo.objects.filter(statut=True))
+        context['contact_info'] = get_object_or_404(models.ContactInfo.objects.filter(statut=True))
+        context['projects'] = get_list_or_404(models.Project.objects.filter(status=True))
+        return context
+        
